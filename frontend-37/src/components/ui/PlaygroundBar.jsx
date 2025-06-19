d3357656d563dc9e4209b52977f97f3457e7faf7
plpl8 <chumarayev@bk.ru>
2025-06-19 17:55:34 +0300

    fix(App, PlaygroundBar, Dashboard): resolve merge conflict

--- frontend-37/src/components/ui/PlaygroundBar.jsx
+++ frontend-37/src/components/ui/PlaygroundBar.jsx
@@ -3,36 +3,41 @@ import { TopBarElement } from "./TopBarElement";
 import { Button } from "./Button";
 import { saveSvg } from "../../assets/save.svg";
 
-export function PlaygroundBar(){
-    return <TopBar style={{backgroundColor: "#6968FF", color: "white"}} contentStyle={{flexBasis: 521 + "px"}}>
-        <TopBarElement>
-            <a href="/">
-                <Button style={
-                        {
-                            width: 52 + "px",
-                            height: 51.15 + "px",
-                            borderRadius: 11 + "px",
-                            backgroundColor: "white"
-                        }
-                    }>
-                    <img src={saveSvg}></img>
-                </Button>
-            </a>
-        </TopBarElement>
-                <TopBarElement>
-            <a href="/playground">
-                <Button style={
-                        {
-                            width: 459 + "px",
-                            height: 51.15 + "px",
-                            borderRadius: 11 + "px",
-                            color: "black",
-                            backgroundColor: "white"
-                        }
-                    }>
-                    PostgreSQL | Template #1
-                </Button>
-            </a>
-        </TopBarElement>
+export function PlaygroundBar() {
+  return (
+    <TopBar
+      style={{ backgroundColor: "#6968FF", color: "white" }}
+      contentStyle={{ flexBasis: 521 + "px" }}
+    >
+      <TopBarElement>
+        <a href="/">
+          <Button
+            style={{
+              width: 52 + "px",
+              height: 51.15 + "px",
+              borderRadius: 11 + "px",
+              backgroundColor: "white",
+            }}
+          >
+            <img src={saveSvg}></img>
+          </Button>
+        </a>
+      </TopBarElement>
+      <TopBarElement>
+        <a href="/playground">
+          <Button
+            style={{
+              width: 459 + "px",
+              height: 51.15 + "px",
+              borderRadius: 11 + "px",
+              color: "black",
+              backgroundColor: "white",
+            }}
+          >
+            PostgreSQL | Template #1
+          </Button>
+        </a>
+      </TopBarElement>
     </TopBar>
+  );
 }
--- frontend-37/src/pages/Dashboard.jsx
+++ frontend-37/src/pages/Dashboard.jsx
@@ -1,40 +1,48 @@
-import dashboardImg from '../assets/dashboardImg.png';
+import dashboardImg from "../assets/dashboardImg.png";
 import humansImg from "../assets/humans.jpg";
 import styles from "./Dashboard.module.css";
+import { MainBar } from "../components/ui/MainBar";
 
 export default function Dashboard() {
-    return (
+  return (
+    <div>
+      <MainBar />
+      <div className={styles.mainDiv}>
         <div>
-            <div className={styles.mainDiv}>
-                <div>
-                    <h1 className={styles.bigTitle}>Interactive databases exercises</h1>
-                    <p className={styles.description}>Improve your skills in managing databases!</p>
-                </div>
-                <img className={styles.dashboardImg} src={dashboardImg} alt="dashboard image" />
-            </div>
-            <div className={styles.rectangleContainer}>
-                <div className={styles.rectangle}>
-                    <img src={humansImg} alt="Humans" />
-                    <div>
-                        <p className={styles.pAboutClassrooms}>Create classrooms</p>
-                        <p className={styles.pAboutAdding}>And add students</p>
-                    </div>
-                </div>
-                <div className={styles.rectangle}>
-                    <img src={humansImg} alt="Humans" />
-                    <div>
-                        <p className={styles.pAboutClassrooms}>Create classrooms</p>
-                        <p className={styles.pAboutAdding}>And add students</p>
-                    </div>
-                </div>
-                <div className={styles.rectangle}>
-                    <img src={humansImg} alt="Humans" />
-                    <div>
-                        <p className={styles.pAboutClassrooms}>Create classrooms</p>
-                        <p className={styles.pAboutAdding}>And add students</p>
-                    </div>
-                </div>
-            </div>
+          <h1 className={styles.bigTitle}>Interactive databases exercises</h1>
+          <p className={styles.description}>
+            Improve your skills in managing databases!
+          </p>
         </div>
-    );
+        <img
+          className={styles.dashboardImg}
+          src={dashboardImg}
+          alt="dashboard image"
+        />
+      </div>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.rectangle}>
+          <img src={humansImg} alt="Humans" />
+          <div>
+            <p className={styles.pAboutClassrooms}>Create classrooms</p>
+            <p className={styles.pAboutAdding}>And add students</p>
+          </div>
+        </div>
+        <div className={styles.rectangle}>
+          <img src={humansImg} alt="Humans" />
+          <div>
+            <p className={styles.pAboutClassrooms}>Create classrooms</p>
+            <p className={styles.pAboutAdding}>And add students</p>
+          </div>
+        </div>
+        <div className={styles.rectangle}>
+          <img src={humansImg} alt="Humans" />
+          <div>
+            <p className={styles.pAboutClassrooms}>Create classrooms</p>
+            <p className={styles.pAboutAdding}>And add students</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
