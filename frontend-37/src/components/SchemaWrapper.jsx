cad90f95d051328b2ecce78287431b7ed373ee50
ustkost <q2zoff.jr@gmail.com>
2025-07-01 17:47:51 +0300

    fix(SchemaWrapper): bug with tabs resizing when resizing window

--- frontend-37/src/components/SchemaWrapper.jsx
+++ frontend-37/src/components/SchemaWrapper.jsx
@@ -6,9 +6,7 @@ import styles from "./SchemaWrapper.module.css";
 export default function SchemaWrapper({ schemas }) {
   const [activeSchema, setActiveSchema] = useState(schemas[0] || null);
 
-  // Синхронизируем активную схему при изменении schemas
   useEffect(() => {
-    // Если активной схемы нет или она больше не существует в schemas
     if (!activeSchema || !schemas.includes(activeSchema)) {
       setActiveSchema(schemas[0] || null);
       for (const schema of schemas) {
@@ -18,15 +16,10 @@ export default function SchemaWrapper({ schemas }) {
         }
       }
     }
-  }, [schemas]); // Зависимость от schemas
+  }, [schemas]);
 
   if (schemas.length === 0) {
-    return (
-      <div
-        className={styles.wrapper}
-        style={{ width: 700, marginLeft: 20, height: 500 }}
-      />
-    );
+    return <div className={styles.wrapper} />;
   }
 
   return (
