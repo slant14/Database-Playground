3419b981e5d1afb8f51cb3fbeac93c7ffcea1531
ustkost <q2zoff.jr@gmail.com>
2025-07-06 21:45:19 +0300

    fix(workflow, docker-compose): add PROD env variable

--- frontend-37/src/const.jsx
+++ frontend-37/src/const.jsx
@@ -1,2 +1,3 @@
-export const API_URL = "http://localhost:8000";
+const isProd = import.meta.env.VITE_PROD === "true";
 
+export const API_URL = isProd ? "https://api.dbpg.ru" : "http://localhost:8000";
