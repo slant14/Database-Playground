c885158b3d748d06a03128722dd429f924ed1fce
Leonid <104693024+I-Leonid-I@users.noreply.github.com>
2025-07-09 16:53:23 +0300

    Fixes applied

--- backend/core/core/urls.py
+++ backend/core/core/urls.py
@@ -48,7 +48,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('test/', include("test.urls")),
     path('template/', include("templates.urls")),
     path('db/', include("db.urls")),
     path('app/', include('core.api_urls')),
