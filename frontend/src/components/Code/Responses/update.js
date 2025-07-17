f654b086908998161369de940f92c7288e718d80
Wal1yS <valera.tinyakov@gmail.com>
2025-07-17 13:09:14 +0300

    fIX

--- frontend/src/components/Code/Responses/update.js
+++ frontend/src/components/Code/Responses/update.js
@@ -18,23 +18,6 @@ class Update extends React.Component {
                                 <Typography.Text className='code-text'>Success! </Typography.Text> <br />
                                 <Typography.Text className='code-text'>Status: <Typography.Text className='code-text' style={{ color: '#fff' }}>Updated</Typography.Text></Typography.Text> <br />
                                 <Typography.Text className='code-text'>Execution time: <Typography.Text className='code-text' style={{ color: '#fff' }}>{this.props.response.execution_time}</Typography.Text></Typography.Text><br />
-                                <Typography.Text className='code-text'>Document info: </Typography.Text><br />
-                                {this.props.response.db_state && this.props.response.db_state.find(doc => doc.id === this.props.response.doc_id) && (
-                                    <div>
-                                        <Typography.Text className='code-text'><ImPointRight /> ID: <Typography.Text className='code-text' style={{ color: '#fff' }}>{this.props.response.doc_id}</Typography.Text></Typography.Text> <br />
-                                        <Typography.Text className='code-text'><ImPointRight /> Title: <Typography.Text className='code-text' style={{ color: '#fff' }}>{this.props.response.db_state.find(doc => doc.id === this.props.response.doc_id).document}</Typography.Text></Typography.Text> <br />
-                                        {this.props.response.db_state.find(doc => doc.id === this.props.response.doc_id).metadata && (
-                                            <div className="metadata-fields">
-                                                {Object.entries(this.props.response.db_state.find(doc => doc.id === this.props.response.doc_id).metadata).map(([key, value]) => (
-                                                    <Typography.Text className='code-text' key={key}>
-                                                        <ImPointRight /> Metadata/{key}: <Typography.Text className='code-text' style={{ color: '#fff' }}>{value}</Typography.Text>
-                                                        <br />
-                                                    </Typography.Text>
-                                                ))}
-                                            </div>
-                                        )}
-                                    </div>
-                                )}
                             </div>
                         }
                     </div>
